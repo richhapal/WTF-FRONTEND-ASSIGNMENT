9fa6cbb35a8ec285527ec9441e89f1764e386864
RICHHAPAL CHOUDHARY <richhapal.16jccs098@jietjodhpur.ac.in>
2023-03-20 19:48:25 +0530

    Add content section

---
+++ src/components/LandingPage/MainContent/ContentSection/ContentSection.jsx
@@ -0,0 +1,7 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+const ContentSection = () => {
+     return <Box w="80%" bg="red" h={350}></Box>;
+};
+export default ContentSection;
