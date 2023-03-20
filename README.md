3bd763b0960d4a1f3a8e5854ea0cbdbff1620490
RICHHAPAL CHOUDHARY <richhapal.16jccs098@jietjodhpur.ac.in>
2023-03-20 13:22:19 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+ <h2 align="center"> WTF FRONTEND ASSIGNMENT</h2>
---
+++ src/components/Landing.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Landing = () => {
+     return (
+          <div>
+               <h2> WTF FRONTEND ASSIGNMENT</h2>
+          </div>
+     );
+};
