86a47149f7e1584a0f5dfbd3cbdd69f8eb82ae75
RICHHAPAL CHOUDHARY <richhapal.16jccs098@jietjodhpur.ac.in>
2023-03-20 22:53:08 +0530

    Add Hero Image in Navbar

--- src/components/LandingPage/Navigation/Navbar.jsx
+++ src/components/LandingPage/Navigation/Navbar.jsx
@@ -1,15 +1,19 @@
-import { Box, Flex, Link, Text } from "@chakra-ui/react";
+import { Box, Flex, Image, Link, Text } from "@chakra-ui/react";
 import React from "react";
 import { NavLink } from "./NavLink";
 import { NavLogo } from "./NavLogo";
 
 const Navbar = () => {
      return (
-          <Box as="nav" bg="blackAlpha.700" h="350px" color="white">
-               <Flex justifyContent="space-between" p="2rem">
-                    <NavLogo />
-                    <NavLink />
-               </Flex>
+          <Box as="nav" bg="blackAlpha.700" h="350px" color="white" position="relative" mb={5}>
+               <Box position="absolute" top="0" w="100%">
+                    <Flex justifyContent="space-between" p="2rem">
+                         <NavLogo />
+                         <NavLink />
+                    </Flex>
+               </Box>
+
+               <Image src={require("../../../images/heroimage.jpg")} height="100%" w="100%" objectFit="cover" />
           </Box>
      );
 };
