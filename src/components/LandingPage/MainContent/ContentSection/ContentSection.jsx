acc2891871efc8eb3429ab3e0cca7b8daa409cfa
RICHHAPAL CHOUDHARY <richhapal.16jccs098@jietjodhpur.ac.in>
2023-03-21 13:11:55 +0530

    Add router link to gym in GymDetailLayout

--- src/components/LandingPage/MainContent/ContentSection/ContentSection.jsx
+++ src/components/LandingPage/MainContent/ContentSection/ContentSection.jsx
@@ -1,14 +1,21 @@
 import { Box, Flex } from "@chakra-ui/react";
 import React from "react";
+import { Link } from "react-router-dom";
 import GymDetailLayout from "./GymDetailLayout";
 
 const ContentSection = () => {
      return (
           <Box w="80%" borderRadius="md">
                <Flex direction="column" gap={6} borderLeft="1px">
-                    <GymDetailLayout />
-                    <GymDetailLayout />
-                    <GymDetailLayout />
+                    <Link to="gym">
+                         <GymDetailLayout />
+                    </Link>
+                    <Link to="gym">
+                         <GymDetailLayout />
+                    </Link>
+                    <Link to="gym">
+                         <GymDetailLayout />
+                    </Link>
                </Flex>
           </Box>
      );
