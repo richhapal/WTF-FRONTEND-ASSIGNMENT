9161cb53d0374252b1bd7a63175d2ae6b77dde80
RICHHAPAL CHOUDHARY <richhapal.16jccs098@jietjodhpur.ac.in>
2023-03-21 12:55:49 +0530

    Add Router In Navigation Bar

--- src/components/LandingPage/Navigation/NavLogo.jsx
+++ src/components/LandingPage/Navigation/NavLogo.jsx
@@ -1,12 +1,25 @@
-import { Box, Image, Link } from "@chakra-ui/react";
+import { Box, Flex, HStack, Image, Text } from "@chakra-ui/react";
 import React from "react";
-
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineArrowLeft } from "react-icons/ai";
 export const NavLogo = () => {
+     const location = useLocation();
      return (
-          <Box boxSize="70px">
-               <Link href="/">
-                    <Image objectFit="cover" alt="wtflogo.png" src={require("../../../images/wtflogo.png")} />
-               </Link>
+          <Box>
+               <Flex alignItems="center" gap={4}>
+                    {location.pathname === "/gym" && (
+                         <Link to="/">
+                              <HStack>
+                                   <AiOutlineArrowLeft />
+                                   <Text>Back</Text>
+                              </HStack>
+                         </Link>
+                    )}
+
+                    <Link to="/">
+                         <Image objectFit="cover" alt="wtflogo.png" src={require("../../../images/wtflogo.png")} height="30px" />
+                    </Link>
+               </Flex>
           </Box>
      );
 };
